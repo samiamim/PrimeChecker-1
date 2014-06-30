e9285c28f5d8128867f7386fea515323b3a6191e
Kasdeus <kasdeus@gmail.com>
2014-06-30 22:16:42 +0100

    Cleaned up code
    * Rewrote the prime checking system
    * Added additional testing (square rooting)

--- prime.py
+++ prime.py
@@ -1,13 +1,18 @@
-num_check = int(input("Enter a number: ")) # Takes the input from the user and sets it to the variable num_check
+import math
 
-if num_check > 1: # Prime numbers are greater than 1 (excluding 1 itself) so this is to filter out numbers less than 1 that cannot be prime
-    for n in range(2,num_check): # Checks for factors
-        if (num_check % n) == 0: # Here, if the division of the input (num_check) is equal to 0 then it cannot be a prime number.
-            print(num_check, "is not a prime number") # If it fails the tests then it cannot be a prime number
-            break
+def prime_checker():
+    num_check = int(input("Enter a number: "))                                              # Takes the input from the user and sets it to the variable num_check
 
-        else:
-            print (num_check, "is a prime number!") #If it passes these tests they it is a prime number
+    sqr_test = math.sqrt(num_check)                                                         # Tests the number by square rooting it, if it returns an integar value then it cannot be a prime number
+    sqr_test = int(sqr_test)
+                                                                                            # Prime numbers are greater than 1 (excluding 1 itself) so this is to filter out numbers less than 1 that cannot be prime
+    for n in range(sqr_test):                                                               # Checks for factors
+        if num_check % (n+2) == 0:                                                          # Here, if the division of the input (num_check) is equal to 0 then it cannot be a prime number.
+            print(num_check, "is not a prime number. It is a factor of ", n+2)              # If it fails the tests then it cannot be a prime number
             break
     else:
-        print (num_check, "is not a prime number as it is equal or less than 1") # If the number is less than or equal to 1 then it is not prime.
+        print(num_check, "is a prime number!")
+
+
+if __name__=='__main__':                                                                    # Runs the program
+    prime_checker()
